8529b5a8d9971c62bc6d2f9283b844287c3a2266
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-08 09:20:59 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+  { year: "2020", result: "W" },
+  { year: "2018", result: "N/A" },
+  { year: "2015", result: "L" },
+];
+
+function superbowlWin(record) {
+  const result = record.find(element => element.result === "W");
+
+  if (result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
+}
+
+console.log(superbowlWin(record));
